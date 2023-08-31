b385cdfc12aaa876f8c3c4d25ddb55ef98b4f046
Indra Mahesa <indramahesa128@gmail.com>
2023-08-31 19:45:01 +0700

    feat: detail product & checkout

---
+++ app/Http/Controllers/Api/TransactionController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Traits\ApiTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class TransactionController extends Controller
+{
+    use ApiTrait;
+
+    public function __construct() {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = env('MIDTRANS_PRODUCTION');
+        \Midtrans\Config::$isSanitized = true;
+        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$appendNotifUrl = env('APP_URL');
+        \Midtrans\Config::$overrideNotifUrl = env('APP_URL');
+        \Midtrans\Config::$paymentIdempotencyKey = env('MIDTRANS_IDEMPOTENCY_KEY');
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'address' => 'required',
+            'phone' => 'required',
+            'quantity' => 'required|numeric',
+            'product_id' => 'required|exists:products,id',
+        ]);
+
+        DB::beginTransaction();
+        
+        try {
+
+            $product = Product::find($request->product_id);
+
+            $transaction = Transaction::create([
+                'invoice_number' => "INV" . date('YmdHis') . rand(1000, 9999),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'price' => $product->selling_price * $request->quantity,
+                'status' => 'pending',
+                'user_id' => Auth::id(),
+            ]);
+            
+            $params = array(
+                'enable_payments' => [
+                    'credit_card', 'mandiri_clickpay', 'cimb_clicks',
+                    'bca_klikbca', 'bca_klikpay', 'bri_epay', 'echannel', 'permata_va',
+                    'bca_va', 'bni_va', 'other_va', 'gopay', 'indomaret',
+                    'danamon_online', 'akulaku'
+                ],
+                'transaction_details' => [
+                    'order_id' => $transaction->invoice_number,
+                    'gross_amount' => $transaction->price,
+                ],
+                'item_details' => [
+                    [
+                        'id' => $product->id,
+                        'name' => $product->name,
+                        'price' => $product->selling_price,
+                        'quantity' => $product->quantity,
+                    ]
+                ],
+                'customer_details' => [
+                    'first_name' => Auth::user()->name,
+                    'phone' => $request->phone,
+                ],
+            );
+
+            $snap = \Midtrans\Snap::createTransaction($params);
+
+            $transaction->payment_url = $snap->redirect_url;
+            $transaction->save();
+
+            DB::commit();
+            
+            return $this->sendResponse([
+                'message' => 'Transaction checkout successfully.',
+                'payment_url' => $snap->redirect_url,
+            ]);
+        } catch (\Exception $e) {
+            return $this->sendResponse($e->getMessage());
+        }
+    }
+
+    public function callback(Request $request) {
+        // $notif = new \Midtrans\Notification();
+
+        // $transaction = $notif->transaction_status;
+        // $fraud = $notif->fraud_status;
+
+        // error_log("Order ID $notif->order_id: "."transaction status = $transaction, fraud staus = $fraud");
+
+
+
+        // if ($transaction == 'capture') {
+        //     if ($fraud == 'challenge') {
+        //     // TODO Set payment status in merchant's database to 'challenge'
+        //     }
+        //     else if ($fraud == 'accept') {
+        //     // TODO Set payment status in merchant's database to 'success'
+        //     }
+        // }
+        // else if ($transaction == 'cancel') {
+        //     if ($fraud == 'challenge') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        //     }
+        //     else if ($fraud == 'accept') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        //     }
+        // }
+        // else if ($transaction == 'deny') {
+        //     // TODO Set payment status in merchant's database to 'failure'
+        // }
+    }
+
+}
---
+++ database/migrations/2023_08_31_101828_create_payment_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('method');
+            $table->longText('headers')->nullable();
+            $table->longText('body')->nullable();
+            $table->foreignId('transaction_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_logs');
+    }
+};
